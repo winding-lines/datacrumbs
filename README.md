197d49360fe5e62ded65739da7a86ec4f891b775
Marius Seritan <git@winding-lines.com>
2020-01-13 21:09:03 -0800

    Better command line parsing

--- README.md
+++ README.md
@@ -6,12 +6,8 @@ you should evaluate a system like [mlflow](https://mlflow.org/)
 ## Overview
 
 The use case where `datacrumbs` is most useful is when the output of your command is a new folder
-during every command execution. The command is providing a very simple chaining capability by creating a `datacrumbs.json` file with the following
-properties:
-
-- `run_time`: UTC time when the command was executed
-- `cwd`: the work folder where the command was executed
-- `git_hash`: current git hash
+during every command execution. The command is providing a very simple chaining capability 
+by creating a `datacrumbs.json` with properties about the run.
 
 
 ## Example
@@ -25,7 +21,7 @@ generate.sh --input /data/some --use_foo --output /data/out/
 To integrate `datacrumbs` you defer the output generation to it
 
 ```
-generate.sh --input /data/some --use_foo --output `datacrumbs /data/out`
+generate.sh --input /data/some --use_foo --output `datacrumbs --base /data/out`
 ```
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,43 @@
 use std::env;
 use std::fs::{create_dir_all, File};
+use std::io::Write;
+use std::path::Path;
 
 use chrono::{DateTime, SecondsFormat, Utc};
 use failure::{err_msg, Fallible};
 use git2::Repository;
 use mac_address::get_mac_address;
 use serde::{Deserialize, Serialize};
-use std::io::Write;
-use std::path::Path;
+use structopt::StructOpt;
 use uuid::v1::{Context, Timestamp};
 use uuid::Uuid;
 
 #[derive(Serialize, Deserialize)]
 struct BreadCrumb {
     cwd: String,
-    run_time: String,
+    id: String,
+    create_time: String,
     git_hash: String,
+    git_repo: String,
 }
 
-fn process(base_folder: &str) -> Fallible<String> {
+#[derive(Debug, StructOpt)]
+#[structopt(about = "Save context about data folder creation")]
+struct Cli {
+    #[structopt(long = "base")]
+    pub base_folder: String,
+}
+
+fn create_output(base_folder: &str) -> Fallible<String> {
     // Gather the context for breadcrumbs
     let path = env::current_dir()?;
     let now: DateTime<Utc> = Utc::now();
-    let git_hash = Repository::open(".")
-        .and_then(|repo| {
-            repo.head()
-                .map(|git_ref| git_ref.target().map(|t| t.to_string()).unwrap_or_default())
-        })
-        .unwrap_or_default();
+    let git_hash = if let Ok(repo) = Repository::discover(".") {
+        let head = repo.head()?.target().map(|s| s.to_string()).unwrap_or_default();
+        head
+    } else {
+        "".to_string()
+    };
 
     // create the output folder in the base folder, use an uuid
     let context = Context::new(1);
@@ -39,14 +49,16 @@ fn process(base_folder: &str) -> Fallible<String> {
     let node_id = get_mac_address()?
         .ok_or(err_msg("missing MAC address"))?
         .bytes();
-    let uuid = Uuid::new_v1(timestamp, &node_id)?;
-    let out_folder = Path::new(base_folder).join(uuid.to_string());
+    let uuid = Uuid::new_v1(timestamp, &node_id)?.to_string();
+    let out_folder = Path::new(base_folder).join(uuid.clone());
     create_dir_all(out_folder.as_path())?;
 
     let bc = BreadCrumb {
         cwd: path.as_os_str().to_str().unwrap().to_string(),
-        run_time: now.to_rfc3339_opts(SecondsFormat::Secs, false),
+        create_time: now.to_rfc3339_opts(SecondsFormat::Secs, false),
+        id: uuid,
         git_hash,
+        git_repo: "".to_string()
     };
     let json = serde_json::to_string_pretty(&bc)?;
     let out_file = out_folder.join("datacrumbs.json");
@@ -59,12 +71,8 @@ fn process(base_folder: &str) -> Fallible<String> {
 }
 
 fn main() -> Result<(), failure::Error> {
-    let mut args = env::args();
-    if args.len() != 2 {
-        return Err(failure::err_msg("Pass in the base folder"));
-    }
-    let base_folder = args.nth(1).ok_or(err_msg("bad base folder"))?;
-    let folder = process(&base_folder)?;
+    let cli = Cli::from_args();
+    let folder = create_output(&cli.base_folder)?;
     println!("{}", folder);
     Ok(())
 }
